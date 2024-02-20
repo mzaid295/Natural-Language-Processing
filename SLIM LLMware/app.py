584d2be40bc5d5beae4acbff71f09d9aaf82053e
Muhammad Zaid <61521923+mzaid295@users.noreply.github.com>
2024-02-20 12:47:56 +0500

    Add files via upload

---
+++ SLIM LLMware/app.py
@@ -0,0 +1,61 @@
+# from llmware.models import ModelCatalog
+import streamlit as st
+
+from llmware_module import (
+    classify_sentiment,
+    detect_emotions,
+    generate_tags,
+    identify_topics,
+    perform_intent,
+    get_ratings,
+    get_category,
+    perform_ner,
+    perform_nli,
+    perform_sql,
+)
+
+# Streamlit app layout
+st.title("Perform NLP Tasks")
+
+# Text input
+text = st.text_area("Enter text here:")
+
+# Analysis tools selection
+analysis_tools = st.multiselect(
+    "Select the analysis tools to use:",
+    ["Sentiment Analysis", "Emotion Detection", "Generate Tags", "Identify Topics",
+     "Perform Intent", "Get Ratings", "Get Category",
+     "Perform NER", "Perform NLI", "Perform SQL"],
+    ["Sentiment Analysis"]  # Default selection
+)
+
+# Execute analysis and display results
+if st.button("Analyze"):
+    results = {}
+
+    if "Sentiment Analysis" in analysis_tools:
+        results["Sentiment Analysis"] = classify_sentiment(text)
+    if "Emotion Detection" in analysis_tools:
+        results["Emotion Detection"] = detect_emotions(text)
+    if "Generate Tags" in analysis_tools:
+        results["Generate Tags"] = generate_tags(text)
+    if "Identify Topics" in analysis_tools:
+        results["Identify Topics"] = identify_topics(text)
+    if "Perform Intent" in analysis_tools:
+        results["Perform Intent"] = perform_intent(text)
+    if "Get Ratings" in analysis_tools:
+        results["Get Ratings"] = get_ratings(text)
+    if "Get Category" in analysis_tools:
+        results["Get Category"] = get_category(text)
+    if "Perform NER" in analysis_tools:
+        results["Perform NER"] = perform_ner(text)
+    if "Perform NLI" in analysis_tools:
+        results["Perform NLI"] = perform_nli(text)
+    # if "Perform SQL" in analysis_tools:
+    #     results["Perform SQL"] = perform_sql(text)
+    if "Perform SQL" in analysis_tools:
+        results["Perform SQL"] = perform_sql(text)
+
+    for tool, response in results.items():
+        st.subheader(tool)
+        st.json(response)
